0a528d8c7f942dccd7cf968c92fc4ea0f82fda50
Tyler Bryant <tbryantdesignsllc@gmail.com>
2024-11-16 18:54:45 -0700

    Updated ContactSection component
    Removed debug input field and added a new phone number input field in the contact section. Also, made minor changes to the bot-field label text.

--- app/components/ContactSection.tsx
+++ app/components/ContactSection.tsx
@@ -88,13 +88,14 @@ export default function ContactSection() {
                     className="px-6 pb-24 pt-20 sm:pb-32 lg:px-8 lg:py-48"
                 >
                     <input type="hidden" name="form-name" value="contact" />
-                    <input type="hidden" name="debug" value="true" />
+
                     <div className="hidden">
                         <label>
-                            Don&apos;t fill this out if you&apos;re human:
+                            Dont fill this out if youre human:{' '}
                             <input name="bot-field" />
                         </label>
                     </div>
+
                     <div className="mx-auto max-w-xl lg:mr-0 lg:max-w-lg">
                         <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
                             <div>
@@ -146,6 +147,24 @@ export default function ContactSection() {
                                         name="email"
                                         type="email"
                                         autoComplete="email"
+                                        required
+                                        className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-jdpestcontrol-500 sm:text-sm sm:leading-6"
+                                    />
+                                </div>
+                            </div>
+                            <div className="sm:col-span-2">
+                                <label
+                                    htmlFor="phone-number"
+                                    className="block text-sm font-semibold leading-6 text-gray-900"
+                                >
+                                    Phone number
+                                </label>
+                                <div className="mt-2.5">
+                                    <input
+                                        id="phone-number"
+                                        name="phone-number"
+                                        type="tel"
+                                        autoComplete="tel"
                                         required
                                         className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-jdpestcontrol-500 sm:text-sm sm:leading-6"
                                     />
